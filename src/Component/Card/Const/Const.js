02764d81aa9fe5bfab59ebaf3dd4ad71a1d796d0
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-02-15 14:03:03 +0530

    Weather APp

---
+++ src/Component/Card/Const/Const.js
@@ -0,0 +1,45 @@
+export const Weather =[
+    {
+        id:1,
+        city:"GorakhPur",
+        Temp:"33 °C",
+        icon:"",
+    },
+    {
+        id:2,
+        city:"Dhanbad",
+        Temp:"33 °C",
+        icon:"",
+    },
+    {
+        id:3,
+        city:"Mumbai",
+        Temp:"33 °C",
+        icon:"",
+    },
+    {
+        id:4,
+        city:"Delhi",
+        Temp:"33 °C",
+        icon:"",
+    },
+    {
+        id:5,
+        city:"Banglore",
+        Temp:"33 °C",
+        icon:"",
+    },
+    {
+        id:6,
+        city:"GorakhPur",
+        Temp:"33 °C",
+        icon:"",
+    },
+    {
+        id:7,
+        city:"GorakhPur",
+        Temp:"33 °C",
+        icon:"",
+    },
+
+]
